11356aeb3777f202f28f9139f67baebf2d255854
Yukio Rivera <DivinusMessor@users.noreply.github.com>
2022-08-17 22:24:16 -0700

    removed test

--- index.js
+++ index.js
@@ -26,38 +26,46 @@ app.listen(3000, () => {
 });
 
 // Routes 
+// To add new class just add a new route 
 app.get("/", function(req, res) {
-    // res.locals.title = "Yukio's Home Page";                    
-    res.render("index.ejs", {title: 'Homepage'}); // THIS LINE IS KEY
+    // res.locals.title = "Yukio's Home Page";  
+    let headerTitle = 'Home';                  
+    res.render("index.ejs", {title: 'Homepage', 'headerTitle': headerTitle}); // THIS LINE IS KEY
 });
 
 app.get("/majorProseminar", function(req, res) {
-    res.render("majorProseminar", {title: 'CST300: Major ProSeminar'})
+    let headerTitle = "CST300: Major ProSeminar";
+    res.render("majorProseminar", {title: 'CST300: Major ProSeminar', 'headerTitle': headerTitle})
 })
 
 app.get("/softwareDesign", function(req, res) {
     // res.locals.title = "CST 338: Software Design";
-    res.render("softwareDesign", {title: 'CST338: Software Design'});
+    let headerTitle = 'CST338: Software Design';
+    res.render("softwareDesign", {title: 'CST338: Software Design', 'headerTitle': headerTitle});
 });
 
 app.get("/databaseManagement", function(req, res) {
     // res.locals.title = "CST 363: Database Management";
-    res.render("databaseManagement", {title: 'CST363: Database Management'});
+    let headerTitle = 'CST363: Database Management';
+    res.render("databaseManagement", {title: 'CST363: Database Management', 'headerTitle': headerTitle});
 });
 
 app.get("/operatingSystems", function(req, res) {
     // res.locals.title = "CST 334: Operating Systems";
-    res.render("operatingSystems", {title: 'CST334: Operating Systems'});
+    let headerTitle = 'CST334: Operating Systems';
+    res.render("operatingSystems", {title: 'CST334: Operating Systems', 'headerTitle': headerTitle});
 });
 
 app.get("/computerNetworking", function(req, res) {
     // res.locals.title = "CST 311: Introduction to Computer Networking";
-    res.render("computerNetworking", {title: 'CST311: Introduction to Computer Networking'});
+    let headerTitle = 'CST311: Introduction to Computer Networking';
+    res.render("computerNetworking", {title: 'CST311: Introduction to Computer Networking', 'headerTitle': headerTitle});
 });
 
 app.get("/internetProgramming", function(req, res) {
     // res.locals.title = "CST 336: Internet Programming";
-    res.render("internetProgramming", {title: 'CST336: Internet Programming'});
+    let headerTitle = 'CST336: Internet Programming';
+    res.render("internetProgramming", {title: 'CST336: Internet Programming', 'headerTitle': headerTitle});
 });
 
 // Classes For website:
